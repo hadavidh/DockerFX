ea44b0d45ca3ae758c17861db1a37ecde71a1964
hadavidh <hadavidh@gmail.com>
2026-04-16 11:17:45 +0200

    configuration de lint

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -9,6 +9,16 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "script"
   },
-  ignorePatterns: ["node_modules/"],
-  rules: {}
+  ignorePatterns: [
+    "node_modules/",
+    "coverage/",
+    "dist/",
+    "build/"
+  ],
+  rules: {
+    "no-empty": "off",
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "no-console": "off"
+  }
 };
