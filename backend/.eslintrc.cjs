d3aa1b084b297203a748a004836b30c399906bca
hadavidh <hadavidh@gmail.com>
2026-04-16 11:00:24 +0200

    ajout des dependances lint dans package json

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "script"
+  },
+  ignorePatterns: ["node_modules/"],
+  rules: {}
+};
